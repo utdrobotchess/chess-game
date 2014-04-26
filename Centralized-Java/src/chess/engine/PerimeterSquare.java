0deca5208e5e028fc38bd820e54dcb7e67d26b67
ryanjmarcotte <ryan@ryanjmarcotte.com>
2014-04-25 21:10:40 -0500

    Updated comments

--- Centralized-Java/src/chess/engine/PerimeterSquare.java
+++ Centralized-Java/src/chess/engine/PerimeterSquare.java
@@ -1,17 +1,18 @@
 package chess.engine;
 
 /**
- *
+ * Makes up the 36 spaces that border and surround a normal chessboard
  * @author Ryan J. Marcotte
  */
 public class PerimeterSquare extends Square {
     private PerimeterSquare() {
-        
+        super();
     }
     
     protected static PerimeterSquare generatePerimeterSquareAt(int location) {
         PerimeterSquare perimSq = new PerimeterSquare();
         perimSq.setLocation(location);
+        perimSq.setOccupant(NullChessPiece.spawnNullChessPieceAt(perimSq));
         return perimSq;
     }
 }
