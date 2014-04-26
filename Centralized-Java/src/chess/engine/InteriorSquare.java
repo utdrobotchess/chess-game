28e3c17f0f6a3829c2f9ee88b8d57bf49cb3fc2a
ryanjmarcotte <ryan@ryanjmarcotte.com>
2014-04-25 21:08:22 -0500

    Updated comments

--- Centralized-Java/src/chess/engine/InteriorSquare.java
+++ Centralized-Java/src/chess/engine/InteriorSquare.java
@@ -1,17 +1,18 @@
 package chess.engine;
 
 /**
- *
+ * Makes up the 64 squares of a normal chessboard
  * @author Ryan J. Marcotte
  */
 public class InteriorSquare extends Square {
     private InteriorSquare() {
-        
+        super();
     }
     
     protected static InteriorSquare generateInteriorSquareAt(int location) {
         InteriorSquare interSq = new InteriorSquare();
         interSq.setLocation(location);
+        interSq.setOccupant(NullChessPiece.spawnNullChessPieceAt(interSq));
         return interSq;
     }
 }
