dff4a6df8558d25932c95f70fd24436530fc1252
ryanjmarcotte <ryan@ryanjmarcotte.com>
2014-04-28 08:29:01 -0500

    Declared a square's neighbors to be final

--- Centralized-Java/src/chess/engine/Square.java
+++ Centralized-Java/src/chess/engine/Square.java
@@ -13,13 +13,14 @@ public abstract class Square implements Comparable<Square> {
     *  6  ***  2
     *  5   4   3
     */
-    private Square[] itsNeighbors = new Square[8];
+    private final Square[] itsNeighbors = new Square[8];
     private ChessPiece itsOccupant;
     
     protected void assignNeighborInDirection(Square newNeighbor, int direction) {
         itsNeighbors[direction] = newNeighbor;
     }
     
+    @Override
     public int compareTo(Square secondSquare) {
         if(itsLocation < secondSquare.getLocation())
             return -1;
