f2a2293a57978a991a7f6fb1b82550e32c205c0d
AbhinavChennapareddy <ch.abhinav.reddy@gmail.com>
2014-08-11 15:43:50 -0500

    updated Temp_Communicv

--- Temp_Communication/CommunicatorAT.java
+++ Temp_Communication/CommunicatorAT.java
@@ -3,50 +3,48 @@ package RobotChess.Communication;
 import gnu.io.*;
 
 import java.io.*;
-import java.math.BigInteger;
 import java.util.*;
 
-	@SuppressWarnings("unused")
-	public class Communicator implements SerialPortEventListener
+
+	public class CommunicatorAT implements SerialPortEventListener
 	{
 	//----------------------------------------------------------------------------------------------------------------------------------------------------
-		/*	Test Program: Write any test programs in the main method.	*/
+		/*	Test Program: Write any test programs for this class in the main method.	*/
 	//----------------------------------------------------------------------------------------------------------------------------------------------------	
 		public static void main(String[] args) throws Exception 
 		{
-			Communicator main = new Communicator();
-			main.InitializeCommunication();
-			char[] crossSquare1  = {0x01, 0x01, 0x07, 0x00, 0x00, 0x00, 0x00, 0x75};
-			char[] crossSquare2  = {0x1c, 0x01, 0x07, 0x00, 0x00, 0x00, 0x00, 0x75};
-			char[] center1  = {0x01, 0x03, 0x2D, 0x02, 0x00, 0x02, 0x00, 0x75};
-			char[] center2  = {0x1c, 0x03, 0x2D, 0x02, 0x00, 0x02, 0x00, 0x75};
-			char[] execute1 = {0x01, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x75};
-			char[] execute2 = {0x1c, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x75};
-			char[] rotate1 = {0x01, 0x02, 0x00, 0x00, 0xB4, 0x00, 0x00, 0x75};
-			char[] rotate2 = {0x1c, 0x02, 0x00, 0x00, 0xB4, 0x00, 0x00, 0x75};
-			
-			SendMessage(crossSquare2);
-			SendMessage(rotate2);
-			SendMessage(crossSquare2);
-			SendMessage(rotate2);
-			SendMessage(center2);
-			
-			long startTime = System.currentTimeMillis(); 
-			while((System.currentTimeMillis() - startTime < 250))
-			{}
-			
-			SendMessage(crossSquare1);
-			SendMessage(rotate1);
-			SendMessage(crossSquare1);
-			SendMessage(rotate1);
-			SendMessage(center1);
-			
-			
-			SendMessage(execute1);
-			SendMessage(execute2);
-
-			
-			
+			CommunicatorAT main = new CommunicatorAT();
+			main.InitializeCommunication("COM26", 57600);
+				char[] crossSquare1 = {0x01, 0x01, 0x07, 0x00, 0x00, 0x00, 0x00, 0x75};
+				char[] rotate1 		= {0x01, 0x02, 0x00, 0x00, 0xB4, 0x00, 0x00, 0x75};
+				char[] center1  	= {0x01, 0x03, 0x2D, 0x02, 0x00, 0x02, 0x00, 0x75};
+				char[] execute1 	= {0x01, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x75};
+				char[] crossSquare2 = {0x1c, 0x01, 0x07, 0x00, 0x00, 0x00, 0x00, 0x75};
+				char[] rotate2 		= {0x1c, 0x02, 0x00, 0x00, 0xB4, 0x00, 0x00, 0x75};
+				char[] center2  	= {0x1c, 0x03, 0x2D, 0x02, 0x00, 0x02, 0x00, 0x75};
+				char[] execute2 	= {0x1c, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x75};
+				
+				long startTime = System.currentTimeMillis(); 
+				while((System.currentTimeMillis() - startTime < 1000))
+				{
+					main.SendMessage(crossSquare1);
+					main.SendMessage(rotate1);
+					main.SendMessage(crossSquare1);
+					main.SendMessage(rotate1);
+					main.SendMessage(center1);
+					
+					if(System.currentTimeMillis() - startTime > 250)
+					{
+						main.SendMessage(crossSquare2);
+						main.SendMessage(rotate2);
+						main.SendMessage(crossSquare2);
+						main.SendMessage(rotate2);
+						main.SendMessage(center2);
+						
+						main.SendMessage(execute1);
+						main.SendMessage(execute2);
+					}
+				}	
 			main.EndCommunication();
 		}		
 		
@@ -56,31 +54,29 @@ import java.util.*;
 		public SerialPort serialPort;
 		
 		// The Port we're normally going to use.
-		private static final String PORT_NAMES[] = {"/dev/ttyUSB0"/*for Linux*/,
-			 										"COM26"/*for Windows*/};
 		public static BufferedReader input;
 		public static OutputStream output;
 		
-		static String receivedMessage = " ";
-		
 		// Time (in Milliseconds) to block while waiting for port to open
 		public static final int TIME_OUT = 0;
-		public static final int BAUD_RATE = 57600;
 		
 	//----------------------------------------------------------------------------------------------------------------------------------------------------	
 		/*	Methods	*/
 	//----------------------------------------------------------------------------------------------------------------------------------------------------	
 		
-		public static void SendMessage(char[] bytearr)
+		public void SendMessage(char[] bytearr)
 		{
 			for(int i = 0; i < 8; i++)
-				Communicator.writeByte(bytearr[i]);
-			//System.out.print("S: ");					//These are for testing purposes.
+				writeByte(bytearr[i]);
+			
+			//----------------------------------//
+			/*	These are for testing purposes.	*/
+			//----------------------------------//
 			//String str = String.format("%040x", new BigInteger(1,new String(bytearr).getBytes()));
-			//PrintString(str.substring(24, str.length()));
+			//System.out.println("S: "+str.substring(24, str.length()));	
 		}
 		
-		private static synchronized void writeByte(char data)
+		private synchronized void writeByte(char data)
 		{
 			try
 			{
@@ -88,39 +84,34 @@ import java.util.*;
 			}
 			catch(Exception e)
 			{
-				System.out.println("[GLaDOS] couldn't write to port. You weren't even testing for that.");
+				PrintString("[GLaDOS] couldn't write to port. You weren't even testing for that.");
 			}
 		}
 		
-		private static void PrintString(String tempString) 
+		public void InitializeCommunication(String COM, int BAUD_RATE)
 		{
-			System.out.println(tempString);
-		}
-		
-		public void InitializeCommunication()
-		{
+			boolean comPortIsFound = false;
 			CommPortIdentifier portID = null;
 			
 			@SuppressWarnings("rawtypes")
 			Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
 			
 			//First, Find an instance of serial port as set in PORT_NAMES.
-			while(portEnum.hasMoreElements())
+			while(portEnum.hasMoreElements()&&!comPortIsFound)
 			{
 				CommPortIdentifier currentPortID = (CommPortIdentifier) portEnum.nextElement();
-				for(String portName: PORT_NAMES)
+				if (currentPortID.getName().equals(COM))
 				{
-					if (currentPortID.getName().equals(portName))
-					{
 						portID = currentPortID;
-						break;
-					}
+						comPortIsFound = true;
 				}
+				else
+					PrintString("COM port "+currentPortID.getName()+" does match entered COM port "+COM);
 			}
 			
 			if(portID == null)
 			{
-				System.out.println("[GLaDOS] specific COM port not found.");
+				PrintString("[GLaDOS] specific COM port not found.");
 			}
 			
 			try
@@ -131,7 +122,6 @@ import java.util.*;
 				// port parameters
 				serialPort.setSerialPortParams(BAUD_RATE,SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
 											   SerialPort.PARITY_NONE);
-				
 				// Open the streams
 				input = new BufferedReader(new InputStreamReader(serialPort.getInputStream()));
 				output = serialPort.getOutputStream();
@@ -142,8 +132,8 @@ import java.util.*;
 			}
 			catch (Exception e)
 			{
-				System.out.println("[GlaDOS] Initialization problem. Debug on your own recognisense.");
-				System.out.println(e.toString());
+				PrintString("[GlaDOS] Initialization problem. Debug on your own recognisense.");
+				System.exit(0);
 			}
 		}
 		
@@ -153,11 +143,20 @@ import java.util.*;
 			{
 				try
 				{
-					receivedMessage = input.readLine();
+					String receivedMessage = input.readLine();
+					PrintString(receivedMessage);
 				}
 				catch(Exception e)
-				{}
+				{
+					PrintString("[Problem] could not write to message.");
+					System.exit(0);
+				}
 			}
+		}
+		
+		private void PrintString(String tempString) 
+		{
+			System.out.println(tempString);
 		}
 			
 		public synchronized void EndCommunication()
