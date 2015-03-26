463b584beda2581a785bed11c52f3efbb815d03c
Omar Hasan <omarhasan777@gmail.com>
2015-03-25 19:38:10 -0500

    Added option to show discovered bots. This feature is currently a prototype and needs to be improved upon.

--- src/edu/utdallas/robotchess/gui/MainFrame.java
+++ src/edu/utdallas/robotchess/gui/MainFrame.java
@@ -13,6 +13,11 @@ import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JMenu;
 import javax.swing.ButtonGroup;
+import javax.swing.JList;
+import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
+
+import java.util.ArrayList;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -20,7 +25,6 @@ import java.awt.event.ActionListener;
 import edu.utdallas.robotchess.manager.UIState;
 import edu.utdallas.robotchess.manager.RobotState;
 import edu.utdallas.robotchess.manager.ApplicationState;
-
 import edu.utdallas.robotchess.robot.RemoteController;
 
 public class MainFrame extends JFrame
@@ -32,30 +36,31 @@ public class MainFrame extends JFrame
     ApplicationState applicationState;
 
     JMenuBar menuBar;
-    
+
     JMenu fileMenu;
     JMenu optionsMenu;
 
     JMenuItem newGameMenuItem;
     JMenuItem exitMenuItem;
-    
+
     JCheckBoxMenuItem enableRobotsMenuItem;
     JRadioButtonMenuItem gameModeMenuItem;
     JRadioButtonMenuItem demoModeMenuItem;
     JRadioButtonMenuItem rcModeMenuItem;
+    JRadioButtonMenuItem robotsDiscoveredMenuItem;
 
     MenuItemListener menuListener;
 
     BoardPanel boardPanel;
-    
-    public MainFrame(ApplicationState applicationState, 
+
+    public MainFrame(ApplicationState applicationState,
                      UIState uiState, RobotState robotState)
     {
         this.applicationState = applicationState;
         this.uiState = uiState;
         this.robotState = robotState;
         uiState.setMainFrame(this);
-        
+
         boardPanel = new BoardPanel(uiState, robotState, 8, 8);
         add(boardPanel);
 
@@ -72,7 +77,7 @@ public class MainFrame extends JFrame
         remove(boardPanel);
         boardPanel = new BoardPanel(uiState, robotState, rows, columns);
         add(boardPanel);
-        
+
         setSize(rows * SQUARE_SIZE, columns * SQUARE_SIZE);
         setLocationRelativeTo(null);
 
@@ -94,23 +99,27 @@ public class MainFrame extends JFrame
         gameModeMenuItem = new JRadioButtonMenuItem("Game Mode");
         demoModeMenuItem = new JRadioButtonMenuItem("Demo Mode");
         rcModeMenuItem = new JRadioButtonMenuItem("RC Mode");
-        
+        robotsDiscoveredMenuItem = new JRadioButtonMenuItem("Discovered Robots");
+
         ButtonGroup bg = new ButtonGroup();
         bg.add(gameModeMenuItem);
         bg.add(demoModeMenuItem);
         bg.add(rcModeMenuItem);
+        bg.add(robotsDiscoveredMenuItem);
 
         gameModeMenuItem.setEnabled(false);
         demoModeMenuItem.setEnabled(false);
         rcModeMenuItem.setEnabled(false);
-        
+        robotsDiscoveredMenuItem.setEnabled(false);
+
         fileMenu.add(newGameMenuItem);
         fileMenu.add(exitMenuItem);
-        
+
         optionsMenu.add(enableRobotsMenuItem);
         optionsMenu.add(gameModeMenuItem);
         optionsMenu.add(demoModeMenuItem);
         optionsMenu.add(rcModeMenuItem);
+        optionsMenu.add(robotsDiscoveredMenuItem);
 
         menuBar.add(fileMenu);
         menuBar.add(optionsMenu);
@@ -121,10 +130,11 @@ public class MainFrame extends JFrame
 
         newGameMenuItem.addActionListener(menuListener);
         exitMenuItem.addActionListener(menuListener);
-        
+
         enableRobotsMenuItem.addActionListener(menuListener);
         demoModeMenuItem.addActionListener(menuListener);
         rcModeMenuItem.addActionListener(menuListener);
+        robotsDiscoveredMenuItem.addActionListener(menuListener);
     }
 
     class MenuItemListener implements ActionListener
@@ -134,27 +144,46 @@ public class MainFrame extends JFrame
         {
             if (e.getSource() == enableRobotsMenuItem) {
                 boolean enable = enableRobotsMenuItem.getState();
+
                 applicationState.setRobotsActive(enable);
+
                 gameModeMenuItem.setEnabled(enable);
                 demoModeMenuItem.setEnabled(enable);
                 rcModeMenuItem.setEnabled(enable);
+                robotsDiscoveredMenuItem.setEnabled(enable);
             }
 
             if (e.getSource() == demoModeMenuItem) {
-                resizeBoard(4, 4); // TODO allow the user to pick the size of the board
+                resizeBoard(4, 8); // TODO allow the user to pick the size of the board
                 boardPanel.initializeDemo();
                 uiState.setDemoMode(true);
             }
-            
+
             if (e.getSource() == rcModeMenuItem) {
                 RemoteController rc = new RemoteController(robotState);
                 rc.start();
             }
 
+            if(e.getSource() == robotsDiscoveredMenuItem){
+                DefaultListModel<String> listModel = new DefaultListModel<String>();
+                ArrayList<String> botList = robotState.getBotList();
+
+                for (String bot : botList) {
+                    listModel.addElement(bot);
+                }
+
+                JFrame frame = new JFrame();
+                frame.setSize(300, 100);
+
+                JList<String> botListJList = new JList<String>(listModel);
+                frame.add(botListJList);
+                frame.setVisible(true);
+            }
+
             if (e.getSource() == exitMenuItem) {
                 // TODO add an "are you sure option"
                 // TODO implement the safe cleanup functionality for the network
-                System.exit(0); 
+                System.exit(0);
             }
         }
     }
