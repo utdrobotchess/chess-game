d5aed3dfd7f3e414d9b7d047390729b2e88cdfc9
Omar Hasan <omarhasan777@gmail.com>
2015-07-21 15:42:26 -0500

    moved chessbotinfo to its own file

---
+++ src/edu/utdallas/robotchess/robotcommunication/ChessbotInfo.java
@@ -0,0 +1,186 @@
+package edu.utdallas.robotchess.robotcommunication;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.rapplogic.xbee.api.XBeeAddress64;
+import com.rapplogic.xbee.api.zigbee.ZNetRxResponse;
+import com.rapplogic.xbee.api.zigbee.ZNetTxRequest;
+
+public class ChessbotInfo
+{
+    XBeeAddress64 xbeeAddress;
+    Integer id;
+    String pieceType;
+    Date lastTimeCommunicated;
+    ZNetRxResponse lastMessageReceived; //Last message received from Chessbot
+    ZNetTxRequest lastMessageSent;      //Last message sent to Chessbot
+    boolean lastMessageDeliveryStatus;
+
+    final int ROOK_IDS[] = {0, 7, 24, 31};
+    final int KNIGHT_IDS[] = {1, 6, 25, 30};
+    final int BISHOP_IDS[] = {2, 5, 26, 29};
+    final int QUEEN_IDS[] = {3, 27};
+    final int KING_IDS[] = {4, 28};
+    final int PAWN_IDS[] = {8, 9, 10, 11, 12, 13, 14, 15,
+                            16, 17, 18, 19, 20, 21, 22, 23};
+
+    public ChessbotInfo(XBeeAddress64 xbeeAddress, Integer id, Date lastTimeCommunicated)
+    {
+        this.xbeeAddress = xbeeAddress;
+        this.lastTimeCommunicated = lastTimeCommunicated;
+
+        setId(id);
+    }
+
+    public XBeeAddress64 getXbeeAddress() {
+        return xbeeAddress;
+    }
+
+    public void setXbeeAddress(XBeeAddress64 xbeeAddress) {
+        this.xbeeAddress = xbeeAddress;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+        setPieceType();
+    }
+
+    public void setPieceType() {
+
+        if (id == null)
+            return;
+
+        for (int rookId : ROOK_IDS) {
+            if (rookId == id) {
+                pieceType = "Rook";
+                return;
+            }
+        }
+
+        for (int knightId : KNIGHT_IDS) {
+            if (knightId == id) {
+                pieceType = "Knight";
+                return;
+            }
+        }
+
+        for (int BishopId : BISHOP_IDS) {
+
+            if (BishopId == id) {
+                pieceType = "Bishop";
+                return;
+            }
+        }
+
+        for (int QueenId : QUEEN_IDS) {
+            if (QueenId == id) {
+                pieceType = "Queen";
+                return;
+            }
+        }
+
+        for (int KingId : KING_IDS) {
+            if (KingId == id) {
+                pieceType = "King";
+                return;
+            }
+        }
+
+        for (int pawnId : PAWN_IDS)
+            if (pawnId == id)
+                pieceType = "Pawn";
+    }
+
+    public String getPieceType() {
+        return pieceType;
+    }
+
+    public Date getLastTimeCommunicated() {
+        return lastTimeCommunicated;
+    }
+
+    public void setLastTimeCommunicated(Date lastTimeCommunicated) {
+        this.lastTimeCommunicated = lastTimeCommunicated;
+    }
+
+    public ZNetTxRequest getLastMessageSent() {
+        return lastMessageSent;
+    }
+
+    public void setLastMessageSent(ZNetTxRequest lastMessageSent,
+                                    boolean deliveryStatus) {
+        this.lastMessageSent = lastMessageSent;
+        this.lastMessageDeliveryStatus = deliveryStatus;
+    }
+
+    public ZNetRxResponse getLastMessageReceived() {
+        return lastMessageReceived;
+    }
+
+    public void setLastMessageReceived(ZNetRxResponse lastMessageReceived) {
+        this.lastMessageReceived = lastMessageReceived;
+    }
+
+    public boolean getLastMessageDeliveryStatus() {
+        return lastMessageDeliveryStatus;
+    }
+
+    public String formatDateToString() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        return dateFormat.format(lastTimeCommunicated);
+    }
+
+    public static String[] getColumnsForTable() {
+        return new String[] {"ID #",
+                            "Piece Type",
+                            "XBee Address",
+                            "Last DeliveryStatus",
+                            "Last Communication Time",
+                            "Last Message Sent To",
+                            "Last Message Recieved From"};
+    }
+
+    public Object[] toObjectArray() {
+        Object[] data = new Object[] {
+        id,
+        pieceType,
+        xbeeAddress,
+        lastMessageDeliveryStatus,
+        formatDateToString(),
+        lastMessageSent,
+        lastMessageReceived};
+
+        return data;
+    }
+
+    //This is messy
+    public String toString() {
+        String string = String.format(id + " " + xbeeAddress +
+                " " + formatDateToString() + " " + lastMessageSent +
+                " " + lastMessageReceived);
+
+        return string;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (o == null || !(o instanceof ChessbotInfo))
+            return false;
+
+        ChessbotInfo that = (ChessbotInfo) o;
+
+        return this.getXbeeAddress().equals(that.getXbeeAddress()) &&
+                this.getId().equals(that.getId()) &&
+                this.getLastTimeCommunicated().equals(that.getLastTimeCommunicated());
+    }
+
+}
